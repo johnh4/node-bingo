b49079ff10a503405dbad0a114175e42a1c3f2be
johnh4 <jehowl4@gmail.com>
2014-01-02 09:44:12 -0500

    Wins bingo officially.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var Bingo = require('./models/bingo');
+var bingo;
 
 var app = express();
 var server = http.createServer(app);
@@ -20,13 +21,13 @@ var client = socketClient.connect('ws://yahoobingo.herokuapp.com');
 
 client.on('connect', function(){
 	console.log('connected!!!!');
-	var board = { slots:
-   { B: [ 4, 7, 10, 11, 13 ],
-     I: [ 20, 22, 25, 33, 36 ],
-     N: [ 37, 45, 46, 52, 54 ],
-     G: [ 55, 59, 60, 62, 69 ],
-     O: [ 73, 74, 76, 77, 78 ] } }
-	var bingo = new Bingo(board);
+	//var board = { slots:
+  // { B: [ 4, 7, 10, 11, 13 ],
+  //   I: [ 20, 22, 25, 33, 36 ],
+  //   N: [ 37, 45, 46, 52, 54 ],
+  //   G: [ 55, 59, 60, 62, 69 ],
+  //   O: [ 73, 74, 76, 77, 78 ] } }
+	//var bingo = new Bingo(board);
 });
 
 var credentials = {
@@ -34,21 +35,33 @@ var credentials = {
 	email: "jehowl4@gmail.com",
 	url: "http://www.github.com/johnh4/node-bingo"
 }
-//client.emit('register', credentials);
 
-client.on('card', function(payload){
-	console.log('payload', payload);
-	
+client.on('connect', function(){
+	console.log('connected!!!!');
+	client.emit('register', credentials);
 });
-
-client.on('number', function(number){
-	console.log('number', number);
-});
-
 client.on('disconnect', function(){
-	console.log('disconnected.');
+	console.log('disconnected!');
 });
-
+client.on('card', function(payload){
+	console.log('card received!');
+	console.log('payload',payload);
+	bingo = new Bingo(payload);
+	console.log('bingo.slots', bingo.slots);
+});
+client.on('win', function(message){
+	console.log('message', message);
+});
+client.on('lose', function(message){
+	console.log('message', message);
+});
+client.on('number', function(num){
+	console.log('num', num,'bingo.taken', bingo.taken);
+	bingo.checkNum(num);
+	if(bingo.checkWin()){
+		client.emit('bingo');
+	}
+});
 // all environments
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
@@ -71,16 +84,6 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-//var client = socketClient.connect('ws://yahoobingo.herokuapp.com');
-
-
 server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-//io.sockets.on('connection', function (socket) {
-//  socket.emit('news', { hello: 'world' });
-//  socket.on('my other event', function (data) {
-//    console.log(data);
-//  });
-//});
